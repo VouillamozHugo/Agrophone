c80ebd14624599a3684664033f2d300cfb8c9132
Tartynn <nathan.dely@gmail.com>
2022-05-16 11:42:33 +0200

    mainActivity code

--- app/src/main/java/com/example/agrophone/UI/MainActivity.java
+++ app/src/main/java/com/example/agrophone/UI/MainActivity.java
@@ -2,15 +2,72 @@ package com.example.agrophone.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.agrophone.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText email_loginView;
+    private EditText passwordView;
+    private Button loginbtn;
+    private Button registerbtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //values from view
+        email_loginView = findViewById(R.id.email_login);
+        passwordView = findViewById(R.id.password);
+        loginbtn = findViewById(R.id.loginbtn);
+        loginbtn.setOnClickListener(view -> attemptLogin());
+
+
+    }
+
+    private void attemptLogin(){
+        email_loginView.setError(null);
+        passwordView.setError(null);
+
+        String email = email_loginView.getText().toString();
+        String pwd = passwordView.getText().toString();
+
+        View focusView = null;
+        boolean cancel = false;
+
+        // Check for a valid password, if the user entered one.
+        if (TextUtils.isEmpty(pwd)) {
+            passwordView.setError("Missing password");
+            passwordView.setText("");
+            focusView = passwordView;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            email_loginView.setError("Email empty");
+            focusView = email_loginView;
+            cancel = true;
+        }
+        if (cancel) {
+            // There was an error don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            //partie base de donnée pour nous connecter
+        }
+
+        public void register(View view) {
+            Intent intent = new Intent(this, AnimationListActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
