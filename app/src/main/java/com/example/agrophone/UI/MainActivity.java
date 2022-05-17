7147af227348d776b3f4cb3c6c1b21a71beb158a
Hugo Vouillamoz <hugo.vouillamoz@students.hevs.ch>
2022-05-17 09:52:20 +0200

    Database structure 09:52

--- app/src/main/java/com/example/agrophone/UI/MainActivity.java
+++ app/src/main/java/com/example/agrophone/UI/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        participantRepo = ((BaseAPP) getApplication()).getParticipantRepo();
         //values from view
         email_loginView = findViewById(R.id.email_login);
         passwordView = findViewById(R.id.password);
@@ -35,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         registerbtn = findViewById(R.id.registerbtn);
         registerbtn.setOnClickListener(view -> register());
 
-        participantRepo = ((BaseAPP) getApplication()).getParticipantRepo();
+
 
 
     }
