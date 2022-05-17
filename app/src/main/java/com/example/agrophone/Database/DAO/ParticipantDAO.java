e8c3cdc88075815087273843c3c1012f50cabb8d
Carlos Daniel Coimbra Soares <danielsoares321@hotmail.com>
2022-05-17 10:08:30 +0200

    DeleteAll

--- app/src/main/java/com/example/agrophone/Database/DAO/ParticipantDAO.java
+++ app/src/main/java/com/example/agrophone/Database/DAO/ParticipantDAO.java
@@ -2,17 +2,25 @@ package com.example.agrophone.Database.DAO;
 
 import android.database.sqlite.SQLiteConstraintException;
 
+import androidx.room.Delete;
 import androidx.room.Insert;
 
 import com.example.agrophone.Database.Entity.Participant;
 
 
 import androidx.room.Dao;
+import androidx.room.Query;
 
 @Dao
 public interface ParticipantDAO {
 
     @Insert
     void insert(Participant participant) throws SQLiteConstraintException;
+
+    @Delete
+    void delete(Participant participant);
+
+    @Query("DELETE FROM participant")
+    void deleteAll();
 
 }
