cedc4956f4de61b3af78aa518374ea0781c8fa56
Tartynn <nathan.dely@gmail.com>
2022-05-23 08:56:53 +0200

    CreateAnimaition initial code 08:56 23.05

--- app/src/main/java/com/example/agrophone/UI/UserAccountActivity.java
+++ app/src/main/java/com/example/agrophone/UI/UserAccountActivity.java
@@ -1,4 +1,34 @@
 package com.example.agrophone.UI;
 
-public class UserAccountActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.agrophone.R;
+
+public class UserAccountActivity extends AppCompatActivity {
+    private TextView userName;
+    private TextView userForname;
+    private TextView userAddress;
+    private TextView userNpa;
+    private TextView userTel;
+    private TextView userEmail;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reservation_list);
+
+        userName = findViewById(R.id.account_name);
+        userForname = findViewById(R.id.account_forname);
+        userAddress = findViewById(R.id.account_address);
+        userNpa = findViewById(R.id.account_npa);
+        userTel = findViewById(R.id.account_tel);
+        userEmail = findViewById(R.id.account_email);
+
+        //juste à attribuer les infos au TextView
+    }
+
+
 }
